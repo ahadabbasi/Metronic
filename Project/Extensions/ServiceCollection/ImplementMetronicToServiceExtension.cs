5a77be5fee3933765757905fa088e88dfe2a75fe
Ahad Abbasi <ahadabbasi@gmail.com>
2023-10-24 11:34:25 +0330

    Create a way to add configuration of metronic in DI

---
+++ Project/Extensions/ServiceCollection/ImplementMetronicToServiceExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ahada.Metronic.Extensions.ServiceCollection;
+
+public static class ImplementMetronicToServiceExtension
+{
+    public static IServiceCollection ImplementMetronicToService(this IServiceCollection collection,
+        IConfiguration configuration)
+    {
+        Startup startup = new Startup();
+
+        return startup.ConfigurationService(collection, configuration);
+    }
+}
---
+++ Project/Startup.cs
@@ -0,0 +1,28 @@
+﻿using Ahada.Metronic.Contracts;
+using Ahada.Metronic.Contracts.Assets;
+using Ahada.Metronic.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Ahada.Metronic;
+
+public class Startup
+{
+    public IServiceCollection ConfigurationService(IServiceCollection services,
+        IConfiguration configuration)
+    {
+        services.AddScoped<IKeenTheme<IKeenAssetFontsScriptsStyles>>(_ =>
+            new KeenTheme(configuration.GetSection(nameof(Metronic)))
+        );
+
+        /*
+        services.AddScoped<IKeenBootstrap, KeenBootstrap>();
+
+        services.AddScoped<IHtmlGenerator, HtmlGenerator>();
+
+        services.AddHttpContextAccessor();
+        */
+        
+        return services;
+    }
+}

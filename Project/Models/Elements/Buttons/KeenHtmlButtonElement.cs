b5d246fe9aa4905b5afca725c1058922da6b561f
Ahad Abbasi <ahadabbasi@gmail.com>
2023-10-27 16:15:29 +0330

    Develop button element just add basic feature

--- Project/Models/Elements/Buttons/KeenHtmlButtonElement.cs
+++ Project/Models/Elements/Buttons/KeenHtmlButtonElement.cs
@@ -1,37 +1,121 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Text.Encodings.Web;
 using Ahada.Metronic.Contracts.Elements.Buttons;
 using Ahada.Metronic.Models.Elements.Abstracts;
 using Ahada.Metronic.Models.Enums;
 using Ahada.Metronic.Models.Enums.Elements;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Ahada.Metronic.Models.Elements.Buttons;
 
-internal class KeenHtmlButtonElement : KeenHtmlElement<IKeenHtmlButtonElement, KeenHtmlButtonElement> , IKeenHtmlButtonElement
+internal class KeenHtmlButtonElement : KeenHtmlElement<IKeenHtmlButtonElement, KeenHtmlButtonElement>,
+    IKeenHtmlButtonElement
 {
+    protected KeenType KeenType { get; set; }
+
+    protected string Url { get; set; }
+
+    protected string IndicatorText { get; set; }
+
+    private string HrefAttributeName 
+        => "href";
+
+    private string TypeAttributeName 
+        => "type";
+
+    public KeenHtmlButtonElement()
+    {
+        Url = string.Empty;
+
+        IndicatorText = string.Empty;
+    }
+
     public void WriteTo(TextWriter writer, HtmlEncoder encoder)
     {
-        throw new NotImplementedException();
+        TagBuilder buttonBuilder = new TagBuilder(string.IsNullOrEmpty(Url) ? "button" : "a");
+
+        MergeClasses("btn", KeenType == KeenType.Custom ? string.Empty : $"btn-{KeenType.ToString().ToLower()}");
+
+        buttonBuilder.MergeAttributes(Attributes);
+
+        IHtmlContentBuilder inner = new HtmlContentBuilder().AppendHtml(InnerHtml);
+
+        if (!string.IsNullOrEmpty(IndicatorText) && string.IsNullOrEmpty(Url))
+        {
+            TagBuilder spanTextIndicator = new TagBuilder("span")
+            {
+                Attributes =
+                {
+                    {
+                        "class", "indicator-label"
+                    }
+                }
+            };
+
+            spanTextIndicator.InnerHtml.AppendHtml(InnerHtml);
+
+            TagBuilder spanProgressIndicator = new TagBuilder("span")
+            {
+                Attributes =
+                {
+                    {
+                        "class", "indicator-progress"
+                    }
+                }
+            };
+
+            spanProgressIndicator.InnerHtml.AppendHtml(new HtmlString(IndicatorText))
+                .AppendHtml(new TagBuilder("span")
+                    { Attributes = { { "class", "spinner-border spinner-border-sm align-middle ms-2" } } });
+
+            inner.Clear()
+                .AppendHtml(spanTextIndicator)
+                .AppendHtml(spanProgressIndicator);
+        }
+
+        buttonBuilder.InnerHtml.AppendHtml(inner);
+
+        buttonBuilder.WriteTo(writer, encoder);
     }
 
     public IKeenHtmlButtonElement Styled(KeenType type)
     {
-        throw new NotImplementedException();
+        KeenType = type;
+
+        return this;
     }
 
     public IKeenHtmlButtonElement Type(KeenButtonType type)
     {
-        throw new NotImplementedException();
+        Attribute.Remove(HrefAttributeName);
+
+        _ = LinkTo(string.Empty);
+        
+        if (type != KeenButtonType.None)
+        {
+            Attribute.Add(TypeAttributeName, type.ToString().ToLower());
+        }
+        
+        return this;
     }
 
     public IKeenHtmlButtonElement LinkTo(string url)
     {
-        throw new NotImplementedException();
+        Url = url;
+
+        Attribute.Remove(TypeAttributeName);
+
+        if (!string.IsNullOrEmpty(url))
+            Attribute.Add(HrefAttributeName, url);
+
+        return this;
     }
 
     public IKeenHtmlButtonElement IsIndicator(string indicatorText)
     {
-        throw new NotImplementedException();
+        IndicatorText = indicatorText;
+
+        return this;
     }
 }

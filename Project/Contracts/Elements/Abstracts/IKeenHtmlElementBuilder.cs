cfaa68b749fbb1b6fc8953ec82567966511a5296
Ahad Abbasi <ahadabbasi@gmail.com>
2023-10-28 21:24:42 +0330

    Create builder for attributes to implement to html element in razor page

---
+++ Project/Contracts/Elements/Abstracts/IKeenHtmlElementBuilder.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Ahada.Metronic.Contracts.Elements.Abstracts;
+
+public interface IKeenHtmlElementBuilder : IKeenHtmlElement<IKeenHtmlElementBuilder>
+{
+    Task<IDictionary<string, string>> GetAs();
+    
+    Task WriteTo(IHtmlHelper html);
+}
---
+++ Project/Models/Elements/Abstracts/KeenHtmlElementBuilder.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using Ahada.Metronic.Contracts.Elements.Abstracts;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Ahada.Metronic.Models.Elements.Abstracts;
+
+internal class KeenHtmlElementBuilder : KeenHtmlElement<IKeenHtmlElementBuilder, KeenHtmlElementBuilder>, IKeenHtmlElementBuilder
+{
+    public Task<IDictionary<string, string>> GetAs()
+    {
+        return Task.FromResult(Attributes);
+    }
+
+    public async Task WriteTo(IHtmlHelper html)
+    {
+        IDictionary<string, string> attributes = await GetAs();
+        
+        await using StringWriter writer = new StringWriter();
+        await writer.FlushAsync();
+        foreach (KeyValuePair<string,string> attribute in attributes)
+        {
+            await writer.WriteAsync($" {attribute.Key}=\"{attribute.Value}\" ");
+        }
+        await html.ViewContext.Writer.WriteAsync(writer.ToString());
+    }
+}

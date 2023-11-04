b31c3284ea616f107f81179f06504430c57268cc
Ahad Abbasi <ahadabbasi@gmail.com>
2023-11-04 09:36:46 +0330

    Create extension for card to be use in the view

---
+++ Project/Extensions/HtmlHelper/Elements/CreateKeenCardExtension.cs
@@ -0,0 +1,11 @@
+﻿using Ahada.Metronic.Contracts.Elements.Cards;
+using Ahada.Metronic.Models.Elements.Cards;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Ahada.Metronic.Extensions.HtmlHelper.Elements;
+
+public static class CreateKeenCardExtension
+{
+    public static IKeenHtmlCardElement CreateKeenCard(this IHtmlHelper html)
+        => new KeenHtmlCardElement(html);
+}

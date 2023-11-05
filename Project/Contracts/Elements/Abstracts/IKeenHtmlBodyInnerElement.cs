5fe6516331465880e1676517d4310a01aaaa4a65
Ahad Abbasi <ahadabbasi@gmail.com>
2023-11-05 09:46:42 +0330

    Create and develop abstract interface and class for body inner

---
+++ Project/Contracts/Elements/Abstracts/IKeenHtmlBodyInnerElement.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+
+namespace Ahada.Metronic.Contracts.Elements.Abstracts;
+
+public interface IKeenHtmlBodyInnerElement<TElement> : IKeenHtmlElement<TElement> 
+    where TElement : IKeenHtmlElement<TElement>, IKeenHtmlBodyInnerElement<TElement>
+{
+    Task<TElement> RenderAsBody<TModel>(string partialName, TModel? model);
+    
+    Task<TElement> RenderAsBody(Task<IHtmlContent> partial);
+    
+    TElement RenderAsBody(IHtmlContent partial);
+}
---
+++ Project/Models/Elements/Abstracts/KeenHtmlBodyInnerElement.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Ahada.Metronic.Contracts.Abstracts;
+using Ahada.Metronic.Contracts.Elements.Abstracts;
+using Microsoft.AspNetCore.Html;
+
+namespace Ahada.Metronic.Models.Elements.Abstracts;
+
+internal abstract class KeenHtmlBodyInnerElement<TSelf, TBase> : KeenHtmlElement<TSelf, TBase>, IKeenHtmlBodyInnerElement<TSelf>
+    where TSelf : IKeenHtmlBodyInnerElement<TSelf>
+    where TBase : KeenHtmlBodyInnerElement<TSelf, TBase>, TSelf 
+    
+{
+    private IKeenPartialRenderer PartialRenderer { get; }
+    
+    protected IHtmlContent Body { get; set; }
+
+    protected KeenHtmlBodyInnerElement(IKeenPartialRenderer partialRenderer)
+    {
+        PartialRenderer = partialRenderer;
+        Body = new HtmlString(string.Empty);
+    }
+
+
+    public async Task<TSelf> RenderAsBody<TModel>(string partialName, TModel? model) =>
+        RenderAsBody(
+            new HtmlString(
+                await PartialRenderer.Render(partialName, model)
+            )
+        );
+
+    public async Task<TSelf> RenderAsBody(Task<IHtmlContent> partial) => 
+        RenderAsBody(await partial);
+
+    public TSelf RenderAsBody(IHtmlContent partial)
+    {
+        Body = partial;
+
+        return (TBase)this;
+    }
+}

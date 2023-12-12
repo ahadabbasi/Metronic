99f5a73a9496c446fcbb0b902a4f7fd5ec764439
Ahad Abbasi <ahadabbasi@gmail.com>
2023-12-12 20:17:04 +0330

    Create extension for alert to be use it in cshtml files

---
+++ Project/Extensions/HtmlHelper/Elements/CreateKeenAlertExtension.cs
@@ -0,0 +1,11 @@
+﻿using Ahada.Metronic.Contracts.Elements.Alerts;
+using Ahada.Metronic.Models.Elements.Alerts;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Ahada.Metronic.Extensions.HtmlHelper.Elements;
+
+public static class CreateKeenAlertExtension
+{
+    public static IKeenHtmlAlertElement CreateKeenAlert(this IHtmlHelper html)
+        => new KeenHtmlAlertElement(html);
+}
